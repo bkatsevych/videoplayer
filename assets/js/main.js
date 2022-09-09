c66d09c1dea29427fed49516714ce288f4ef8adb
Tot3sMalot3s <bkatsevych@icloud.com>
2022-09-09 18:22:09 +0300

    play/pause btn fix, progress bar functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,24 @@
 const container = document.querySelector(".container");
 const mainVideo = container.querySelector("video");
 const playPauseBtn = container.querySelector(".play-pause i");
+const progressBar = container.querySelector(".progress-bar");
 
 // play/pause functionality
 playPauseBtn.addEventListener("click", () => {
     mainVideo.paused ? mainVideo.play() : mainVideo.pause();
 
     // change button's icon
-    playPauseBtn.classList.contains("fa-play")
-        ? playPauseBtn.classList.replace("fa-play", "fa-pause")
-        : playPauseBtn.classList.replace("fa-pause", "fa-play");
+    mainVideo.addEventListener("play", () => {
+        playPauseBtn.classList.replace("fa-play", "fa-pause");
+    });
+    mainVideo.addEventListener("pause", () => {
+        playPauseBtn.classList.replace("fa-pause", "fa-play");
+    });
+});
+
+// progress bar functionality
+mainVideo.addEventListener("timeupdate", (e) => {
+    let { currentTime, duration } = e.target;
+    let ratio = (currentTime / duration) * 100; // % of currentTime from duration
+    progressBar.style.width = `${ratio}%`; // displaying respective progress bar width
 });
